726a363d6e50553daadeefa1d83ce732f091c9a1
ZyuDev <mikhazyu@gmail.com>
2026-04-14 09:54:22 +0300

    Project created

---
+++ src/BaSys.MetricsCounter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BaSys.MetricsCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
